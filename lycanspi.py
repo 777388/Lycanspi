4c8741cc63cd6b2555ca4e16eb9b32a97884f434
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-02-18 10:48:19 -0800

    Update lycanspi.py

--- lycanspi.py
+++ lycanspi.py
@@ -4,14 +4,14 @@ class superpi:
     def __init__(self, instance):
         self.instance = instance
     def lycanspi(self):
-        return self.instance-self.instance/(float(self.instance)*2.314), (self.instance-self.instance/(float(self.instance)*2.314))-self.instance
+        return [self.instance-self.instance/(float(self.instance)*2.314), (self.instance-self.instance/(float(self.instance)*2.314))-self.instance]
 class run(superpi):
     def __init__(self, instance):
         super(run, self).__init__(instance)
 
-
 for i in str(matplotlib.inspect.tokenize.PseudoToken):
     allocator = run(int(ord(i)))
-    allocator.lycanspi()
-allocator = run(int(sys.argv[1]))
-print(allocator.lycanspi())
+    print(allocator.lycanspi())
+    list(map(lambda x: print(matplotlib.numpy.eye(int(x)), matplotlib.numpy.broadcast(int(x))), allocator.lycanspi()))
+    allocator = run(int(sys.argv[1]))
+    print(allocator.lycanspi())
